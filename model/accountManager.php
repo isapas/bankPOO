3ec1fc28a1fd842c697235a612947ed6e523893e
isapas <isabelle.passeniers@gmail.com>
2019-01-17 12:10:40 +0100

    manager

--- model/accountManager.php
+++ model/accountManager.php
@@ -3,8 +3,13 @@
 	class AccountManager {
 		protected $db;
 
-		public function getAccount($id) {
-			
+		public function getAccount($data) {
+			$query = $this->getDb()->query('SELECT * FROM Account');
+			$data = $query->fetchall(PDO::FETCH_ASSOC);
+				foreach($data as $key => $account) {
+					$data[$key] = new Account($account);
+				}
+			return $data;
 		}
 		
 	}
